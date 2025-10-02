4675a5c1042935bd03ebbe326d922bd423aeacc1
Ramchandra Karki <106328401+SilentBuddha1@users.noreply.github.com>
2025-10-02 14:37:26 +0000

    Previous

--- NodeJS/Sathi/src/app.js
+++ NodeJS/Sathi/src/app.js
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+app.use("/test", (req, res) => {
+    res.send("Test route is working!");
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });

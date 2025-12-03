c85285e074a6c15313f997621c0c3ba19f8266d5
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-12-03 16:08:17 +0545

    route

--- NodeJS/backendYT/src/server.js
+++ NodeJS/backendYT/src/server.js
@@ -1,9 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+import 'dotenv/config';
+import express from 'express';
+
+import movieRoutes from "./routes/movieRoutes.js";
 
 const app = express();
 
-const PORT = Number(process.env.PORT);
+app.use(express.json());
+app.use("/movies", movieRoutes);
+
+app.get('/', (req,res) => {
+   res.json({message: "Hello World"});
+})
+
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
